8faeca23cc01d0f9e1441fe43665cdda43dcbe1a
kostia-stepanenko <stepanenko.kotia@gmail.com>
2023-01-05 13:59:17 -0100

    gg

--- src/TestingSomeStuff/ListOfCommands.java
+++ src/TestingSomeStuff/ListOfCommands.java
@@ -2,13 +2,16 @@ package TestingSomeStuff;
 
 import java.lang.*;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ListOfCommands
 {
 
 
        public static void main (String[] args) {
-           //messing with values and to_string
+           /**messing with values and to_string**/
            int x = 42;
            String fromX = Integer.toString(x);
            boolean b1 = false;
@@ -19,7 +22,7 @@ public class ListOfCommands
            //System.out.println(fromLong);
 
 
-//String builder fun
+/**String builder fun**/
 
            // StringBuilder sb1 = new StringBuilder("Hello there");
            // sb1.append(",");
@@ -29,7 +32,7 @@ public class ListOfCommands
            //sb1.delete(0,sb1.length());
 
 
-           //Scanner use
+           /**Scanner use**/
 
            //Scanner sc1 = new Scanner(System.in);
            // System.out.println("Input value");
@@ -37,8 +40,77 @@ public class ListOfCommands
            //System.out.println(input);
 
 
-           //Itteration loop
+           /**Iteration loop**/
            // for (int i = 0; i < 0/* array sie, string length,*/ ; i ++){}
 
-       }
+
+           /** Reversed number**/
+
+
+//           public int getReverse() {
+//        int temp = (int) x;
+//        List<Integer> digitsList = new ArrayList<>();
+//
+//        int reverseDigit = 0;
+//
+//        while (temp != 0) {
+//            reverseDigit = temp % 10;
+//            temp = temp / 10;
+//            digitsList.add(reverseDigit);
+//        }
+//
+//
+//        int res = 0;
+//        for(int val : digitsList ){
+//           res = res * 10 + val;
+//        }
+//
+//        return res;
+//    }
+
+
+
+
+           /** Sum of digits of number**/
+//           public int sumDigits() {
+//               int sum = 0;
+//               int temp = (int) x;
+//
+//               while (temp != 0) {
+//                   int digit = temp % 10;
+//                   temp = temp / 10;
+//                   sum = sum + digit;
+//               }
+//
+//               return sum;
+//           }
+//       }
+
+
+
+           /**List of factors of number**/
+
+//           public List<Integer> listFactor(){
+//
+//               int val = (int)x;
+//
+//               List<Integer> factorList = new ArrayList<>();
+//
+//                val = 120, factorList = []
+//
+//               while(val != 1) {
+//
+//                   for(int i = 2; i <= val; ++i){
+//                       int rem = val % i;
+//                       if (rem == 0){
+//                           factorList.add(i);
+//                           val = val /i;
+//                           break;
+//                       }
+//                   }
+//               }
+//
+//               return factorList;
+//           }
+      }
 }
